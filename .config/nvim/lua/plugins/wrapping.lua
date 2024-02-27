9ccefe7be9c9ff35739487ac1ae7d216a84e6dfa
Khalid <khalid.hamu@gmail.com>
2024-02-26 23:50:07 -0700

    sway tweaks and nvim tweaks

--- .config/nvim/lua/plugins/wrapping.lua
+++ .config/nvim/lua/plugins/wrapping.lua
@@ -1,13 +1,16 @@
 return {
-  "andrewferrier/wrapping.nvim",
-  keys = function()
-    return {
-      { "<leader>ws", "<cmd>SoftWrapMode<cr>", desc = "Soft Wrap" },
-      { "<leader>wh", "<cmd>HardWrapMode<cr>", desc = "Hard Wrap" },
-      { "<leader>wt", "<cmd>ToggleWrapMode<cr>", desc = "Toggle Wrap" },
-    }
-  end,
-  config = function()
-    require("wrapping").setup()
-  end,
+	"andrewferrier/wrapping.nvim",
+	config = function()
+		require("wrapping").setup()
+	end,
+	keys = function()
+		return {
+			{ "<leader>ws", "<cmd>SoftWrapMode<cr>", desc = "Soft Wrap" },
+			{ "<leader>wh", "<cmd>HardWrapMode<cr>", desc = "Hard Wrap" },
+			{ "<leader>wt", "<cmd>ToggleWrapMode<cr>", desc = "Toggle Wrap" },
+		}
+	end,
+	opts = {
+		notify_on_switch = false,
+	},
 }
